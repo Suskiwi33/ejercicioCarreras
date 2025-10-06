5c4c9e9457348af18035dcb1494b0f383844ea4f
Alex Borda <alexvictorb@gmail.com>
2025-10-06 13:50:25 +0200

    conection update

--- db_conexion.py
+++ db_conexion.py
@@ -9,6 +9,8 @@ def coneccion_bd(user, password):
             database="carreras"
         )
         if connection.is_connected():
+            print("✅ Conexión establecida con la base de datos.")
             return connection
     except mysql.connector.Error as err:
+        print(f"❌ Error al conectar con MySQL: {err}")
         return None
